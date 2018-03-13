2a93ac9cdfd3d6e5b493c09f9a6baa43d99df550
Srikanth Bandaru <srikanthb602@gmail.com>
2018-03-13 13:31:34 -0500

    Left align breadcrumbs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -15,81 +15,79 @@ export default function Header(props) {
 			<header className="global-header-wrapper">
 				<div className="mega-menu-wrapper">
 					<Row className="mega-menu-row">
-						<Col>
-							<div className="no-pad">
-								{
-									props.location.pathname === '/' ?
-										<Breadcrumb>
-											<Breadcrumb.Item active href= {`${process.env.PUBLIC_URL}/`}>
-												Home
-											</Breadcrumb.Item>
-										</Breadcrumb>
-										:
-										null
-								}
-								{
-									props.location.pathname === '/createTable' ?
-										<Breadcrumb>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
-												Home
-											</Breadcrumb.Item>
-											<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/createTable`}>
-												createTable
-											</Breadcrumb.Item>
-										</Breadcrumb>
-										:
-										null
-								}
-								{
-									props.location.pathname === '/enterFieldsInfo' ?
-										<Breadcrumb>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
-												Home
-											</Breadcrumb.Item>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/createTable`}>
-												createTable
-											</Breadcrumb.Item>
-											<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/enterFieldsInfo`}>
-												enterFieldsInfo
-											</Breadcrumb.Item>
-										</Breadcrumb>
-										:
-										null
-								}
-								{
-									props.location.pathname === '/enterTableValues' ?
-										<Breadcrumb>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
-												Home
-											</Breadcrumb.Item>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/createTable`}>
-												createTable
-											</Breadcrumb.Item>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/enterFieldsInfo`}>
-												enterFieldsInfo
-											</Breadcrumb.Item>
-											<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/enterTableValues`}>
-												enterTableValues
-											</Breadcrumb.Item>
-										</Breadcrumb>
-										:
-										null
-								}
-								{
-									props.location.pathname === '/updateTable' ?
-										<Breadcrumb>
-											<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
-												Home
-											</Breadcrumb.Item>
-											<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/updateTable`}>
-												View/Update Table
-											</Breadcrumb.Item>
-										</Breadcrumb>
-										:
-										null
-								}
-							</div>
-						</Col>
+						<div className="no-pad">
+							{
+								props.location.pathname === '/' ?
+									<Breadcrumb>
+										<Breadcrumb.Item active href= {`${process.env.PUBLIC_URL}/`}>
+											Home
+										</Breadcrumb.Item>
+									</Breadcrumb>
+									:
+									null
+							}
+							{
+								props.location.pathname === '/createTable' ?
+									<Breadcrumb>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
+											Home
+										</Breadcrumb.Item>
+										<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/createTable`}>
+											createTable
+										</Breadcrumb.Item>
+									</Breadcrumb>
+									:
+									null
+							}
+							{
+								props.location.pathname === '/enterFieldsInfo' ?
+									<Breadcrumb>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
+											Home
+										</Breadcrumb.Item>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/createTable`}>
+											createTable
+										</Breadcrumb.Item>
+										<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/enterFieldsInfo`}>
+											enterFieldsInfo
+										</Breadcrumb.Item>
+									</Breadcrumb>
+									:
+									null
+							}
+							{
+								props.location.pathname === '/enterTableValues' ?
+									<Breadcrumb>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
+											Home
+										</Breadcrumb.Item>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/createTable`}>
+											createTable
+										</Breadcrumb.Item>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/enterFieldsInfo`}>
+											enterFieldsInfo
+										</Breadcrumb.Item>
+										<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/enterTableValues`}>
+											enterTableValues
+										</Breadcrumb.Item>
+									</Breadcrumb>
+									:
+									null
+							}
+							{
+								props.location.pathname === '/updateTable' ?
+									<Breadcrumb>
+										<Breadcrumb.Item href = {`${process.env.PUBLIC_URL}/`}>
+											Home
+										</Breadcrumb.Item>
+										<Breadcrumb.Item active href = {`${process.env.PUBLIC_URL}/updateTable`}>
+											View/Update Table
+										</Breadcrumb.Item>
+									</Breadcrumb>
+									:
+									null
+							}
+						</div>
 					</Row>
 				</div>
 			</header>
